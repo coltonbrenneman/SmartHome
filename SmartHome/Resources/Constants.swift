bed5d37d4e1de4243339f6274e5f204db8d361d1
Colton Brenneman <coltonbrenneman@Coltons-MacBook-Air.local>
2023-02-16 14:29:23 -0700

    finished viewController and constants

---
+++ SmartHome/Resources/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  SmartHome
+//
+//  Created by Colton Brenneman on 2/16/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Notifications {
+        
+        static let turnOnAllnNotification = Notification.Name("turnOnAllDevicesNotification")
+        static let turnAllOffNotification = Notification.Name("turnOffAllDevicesNotification"')
+    }
+}
---
+++ SmartHome/ViewController/ToggleAllDevicesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ToggleAllDevicesViewController.swift
+//  SmartHome
+//
+//  Created by Colton Brenneman on 2/16/23.
+//
+
+import UIKit
+
+class ToggleAllDevicesViewController: UIViewController {
+
+    //MARK: - Actions
+    
+    @IBAction func turnAllOnButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func turnAllOffButtonTapped(_ sender: Any) {
+    }
+    
+    
+    
+}
